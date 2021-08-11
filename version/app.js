02c1ba47725d7025c4ab1123814fe64f6766beaa
anonymous5555555555555555 <hhenkemans@svb.nl>
2021-08-11 15:23:48 +0200

    vijf

--- version/app.js
+++ version/app.js
@@ -3,9 +3,9 @@ app = express();
 
 var response;
 
-app.get('/test', function (req, res) {
+app.get('/', function (req, res) {
 
-    response = 'This is version 4 of the app.' + '\n';
+    response = 'This is version 5 of the app.' + '\n';
 
     //send the response to the client
     res.send(response);
